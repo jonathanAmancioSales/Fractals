7620aa7909fc4d37a05536a8edfe48323853a7b0
Jonathan <jonathan.sales@fisica.ufc.br>
2023-04-30 17:18:56 -0300

    style: Fix spaces

--- Sierpinski_Triangle/Chaos_Game_method/Sierpinski_Triangle_Chaos_Game.cpp
+++ Sierpinski_Triangle/Chaos_Game_method/Sierpinski_Triangle_Chaos_Game.cpp
@@ -21,44 +21,44 @@ double RandomNumber(double Min, double Max) {
 }
 
 int main() {
-	int v, i;
-	long double x[4], y[4], r;
-	long double Xnew, Ynew, Xold, Yold;
+    int v, i;
+    long double x[4], y[4], r;
+    long double Xnew, Ynew, Xold, Yold;
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	x[0] = y[0] = 0; x[1] = 1; y[1] = 0; x[2] = 0.5; y[2] = r3_2;
+    x[0] = y[0] = 0; x[1] = 1; y[1] = 0; x[2] = 0.5; y[2] = r3_2;
 
-	r = RandomNumber(0, 1);
-	x[3] = 1 - r;
+    r = RandomNumber(0, 1);
+    x[3] = 1 - r;
 
-	cout << "r = " << r << endl;
+    cout << "r = " << r << endl;
 
-	r = RandomNumber(0, 1);
-	double t = 1 - r;
-	double w = (60*M_PI)/180;
+    r = RandomNumber(0, 1);
+    double t = 1 - r;
+    double w = (60*M_PI)/180;
 
-	if(x[3] < 0.5){ y[3] = x[3] * tan(w) * t; }
-	else{ y[3] = (1-x[3])*tan(w)*t; }
+    if(x[3] < 0.5){ y[3] = x[3] * tan(w) * t; }
+    else{ y[3] = (1-x[3])*tan(w)*t; }
 
-	Xold = x[3];
-	Yold = y[3];
+    Xold = x[3];
+    Yold = y[3];
 
-	ofstream result ("Sierpinski_Triangle_pontos.txt");
-	for(i = 0; i <= 3; i++)
-	{ result << x[i] << "\t" << y[i] << "\n"; }
+    ofstream result ("Sierpinski_Triangle_pontos.txt");
+    for(i = 0; i <= 3; i++)
+    { result << x[i] << "\t" << y[i] << "\n"; }
 
-	for(i = 4; i < n; i++)
-	{
-		v = rand()%3;
+    for(i = 4; i < n; i++)
+    {
+        v = rand()%3;
 
-		Xnew = (Xold + x[v])/2;
-		Ynew = (Yold + y[v])/2;
-		result << setprecision(20) << Xnew << "\t" << Ynew << "\n";
+        Xnew = (Xold + x[v])/2;
+        Ynew = (Yold + y[v])/2;
+        result << setprecision(20) << Xnew << "\t" << Ynew << "\n";
 
-		Xold = Xnew;
-		Yold = Ynew;
-	}
+        Xold = Xnew;
+        Yold = Ynew;
+    }
 
-	return 0;
+    return 0;
 }
